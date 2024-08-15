6c19a7df6c02cf113be3f84412cd651d3c301e01
Tapas Tripathi <tapas.nilanjit@gmail.com>
2024-08-15 10:21:31 +0000

    update gitignore

--- __lwr_cache__/lwc_compiled_modules/cache/my_blogPostsList_blogPostsList_css_0_0_1.js
+++
@@ -1,8 +0,0 @@
-function stylesheet(token, useActualHostSelector, useNativeDirPseudoclass) {
-  var shadowSelector = token ? ("[" + token + "]") : "";
-  var hostSelector = token ? ("[" + token + "-host]") : "";
-  var suffixToken = token ? ("-" + token) : "";
-  return ".post-title-section" + shadowSelector + " {padding: 1rem 0rem;}.posts" + shadowSelector + " {height: 100vh;}.posts" + shadowSelector + " h1" + shadowSelector + " {margin: 1.25rem 1.25rem 1.5rem 0rem;border-bottom: black;border-bottom: 0.25rem solid black;}";
-  /*LWC compiler v7.2.1*/
-}
-export default [stylesheet];
--- __lwr_cache__/lwc_compiled_modules/cache/my_post_post_css_0_0_1.js
+++
@@ -1,8 +0,0 @@
-function stylesheet(token, useActualHostSelector, useNativeDirPseudoclass) {
-  var shadowSelector = token ? ("[" + token + "]") : "";
-  var hostSelector = token ? ("[" + token + "-host]") : "";
-  var suffixToken = token ? ("-" + token) : "";
-  return "header" + shadowSelector + " {background: #333;color: #fff;padding: 1rem;}nav" + shadowSelector + " a" + shadowSelector + " {color: #fff;margin: 0 1rem;}main" + shadowSelector + " {padding: 1rem;}.markdown-body" + shadowSelector + " {box-sizing: border-box;min-width: 200px;max-width: 980px;margin: 0 auto;padding: 45px;}";
-  /*LWC compiler v7.2.1*/
-}
-export default [stylesheet];
